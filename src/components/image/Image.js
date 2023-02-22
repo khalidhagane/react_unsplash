cda4f0235fd97e733f0d5fa7b0ce4934f706adc7
khalid hagane <khalidhagane62@gmail.com>
2023-02-22 21:30:34 +0100

    mettre à jour les composants ( - balisage et style - gérer les changements d'entrée de recherche )

---
+++ src/components/image/Image.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Image({ id, alt_description, urls }) {
+  return (
+    <div key={id} className='flex w-1/3 flex-wrap'>
+      <div className='w-full p-1 md:p-2'>
+        <img
+          alt={alt_description}
+          className='block h-full w-full rounded-lg object-cover object-center'
+          src={urls.small}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Image;
--- src/components/search/SearchComponent.js
+++ src/components/search/SearchComponent.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React from 'react';
 
-export default function SearchComponent() {
-    return (
-        <div className="flex items-center">
-            <div className="flex  rounded">
-                <input
-                    type="text"
-                    className="block w-full indent- px-4 py-2 text-Zinc-900 bg-white border rounded-md focus:border-sky-400 focus:ring-sky-300 focus:outline-none focus:ring-2 focus:ring-opacity-40"
-                    placeholder="Search..."
-                />
-                <button className="px-4 text-white bg-sky-600 border-l rounded ">
-                    Search
-                </button>
-            </div>
+export default function SearchComponent({ setSearch, getData }) {
+  return (
+    <div className='flex items-center bg-slate-900 py-10'>
+      <div className='max-w-md mx-auto w-full'>
+        <div className='flex items-center'>
+          <div className='flex  rounded'>
+            <input
+              type='text'
+              className='block w-full indent- px-4 py-2 text-Zinc-900 bg-white border rounded-md focus:border-sky-400 focus:ring-sky-300 focus:outline-none focus:ring-2 focus:ring-opacity-40'
+              placeholder='Search...'
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <button
+              onClick={getData}
+              type='button'
+              className='px-4 text-white bg-sky-600 border-l rounded '
+            >
+              Search
+            </button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
