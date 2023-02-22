0df2cd4e6266ccb2cf17c16e4910f6878da0c811
khalid hagane <khalidhagane62@gmail.com>
2023-02-22 21:26:33 +0100

    configurer le balisage et le style initiaux pour le composant de recherche et d'image et également configurer le crochet axios

--- src/components/search/SearchComponent.js
+++ src/components/search/SearchComponent.js
@@ -3,10 +3,10 @@ import React from "react";
 export default function SearchComponent() {
     return (
         <div className="flex items-center">
-            <div className="flex border border-sky-200 rounded">
+            <div className="flex  rounded">
                 <input
                     type="text"
-                    className="block w-full indent- px-4 py-2 text-sky-700 bg-white border rounded-md focus:border-sky-400 focus:ring-sky-300 focus:outline-none focus:ring-2 focus:ring-opacity-40"
+                    className="block w-full indent- px-4 py-2 text-Zinc-900 bg-white border rounded-md focus:border-sky-400 focus:ring-sky-300 focus:outline-none focus:ring-2 focus:ring-opacity-40"
                     placeholder="Search..."
                 />
                 <button className="px-4 text-white bg-sky-600 border-l rounded ">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,36 @@
-import React from 'react'
+import {createContext,useState} from 'react'
 import HeaderComponent from '../components/header/HeaderComponent'
+import ImagesComponent from '../components/images/ImageComponent'
+import useAxios from '../hooks/useAxios';
+
 // import SearchComponent from '../components/search/SearchComponent'
 
+//creat context
+export const ImageContext = createContext();
+
 function Home() {
+    const [searchImage, setSearchImage] = useState('');
+    const { response, isLoading, error, fetchData } = useAxios(`search/photos?page=1&query=office&client_id=${process.env.REACT_APP_MY_ACCESS_KEY}`);
+
+    console.log(process.env.REACT_APP_MY_ACCESS_KEY)
+
+    console.log(response)
+    const value = {
+        response,
+        isLoading,
+        error,
+        fetchData,
+        // searchImage,
+        // setSearchImage
+      }
+
   return (
-    <div className='bg-slate-900'>
+    <div className='bg-slate-800'>
+    <ImageContext.Provider value={value}>
         
     <HeaderComponent/>
+    <ImagesComponent/>
+    </ImageContext.Provider>
     </div>
   )
 }
